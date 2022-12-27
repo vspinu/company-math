b6d2b77ff371bc11af2f4fda4fcf9c75610841cd
Vitalie Spinu <spinuvit@gmail.com>
2022-12-27 14:27:40 +0100

    Typo: use correct variable name during initalization

--- company-math.el
+++ company-math.el
@@ -68,7 +68,7 @@ in a new Emacs session."
   (regexp-quote company-math-symbol-prefix))
 (defvar company-math--unicode-prefix-regex
   (let ((psym (regexp-quote company-math-symbol-prefix))
-        (psub (when company-math-symbol-prefix
+        (psub (when company-math-subscript-prefix
                 (concat "\\|" (regexp-quote company-math-subscript-prefix))))
         (psup (when company-math-superscript-prefix
                 (concat "\\|" (regexp-quote company-math-superscript-prefix)))))
